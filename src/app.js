2105d3f39c1b7243fbaecf6a4fac4499e56516b5
kiranM8497 <kiranmuttanwar@gmail.com>
2025-08-24 20:15:12 +0530

    User Schema , Model and REST api calls

--- src/app.js
+++ src/app.js
@@ -3,19 +3,88 @@ const { adminAuth, userAuth } = require("./middlewares/auth");
 const { connectDb } = require("./config/database");
 const User = require("./models/user");
 const app = express();
-
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  console.log(User);
-  //creatign a new instacne of a user model
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "king@google.com",
-    password: "king@123",
-  });
+  try {
+    console.log(req.body);
+    const data = req.body;
+    //creatign a new instacne of a user model
+    const user = new User(data);
 
-  await user.save();
-  res.send("User added Successfully");
+    await user.save();
+    res.send("User added Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const { emailId } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(user);
+    }
+    // const users = await User.find({ emailId: emailId });
+
+    // if (users.length === 0) {
+    //   res.status(404).send("User Not Found");
+    // }
+    // res.send(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
+});
+
+//delete a user from DB
+app.delete("/user", async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    const result = await User.findByIdAndDelete(id);
+    // const result = await User.findByIdAndDelete({ _id: id });
+    // const result = await User.deleteOne({ _id: id });
+    // User.deleteOne({ _id: id }).exec();
+
+    // console.log(result);
+    res.send("user deleted successfully!!");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const { id, emailId } = req.body;
+
+    const result = await User.findByIdAndUpdate(id, {
+      $set: { emailId: emailId },
+    });
+    // const result = await User.findByIdAndDelete({ _id: id });
+    // const result = await User.deleteOne({ _id: id });
+    // User.deleteOne({ _id: id }).exec();
+
+    // console.log(result);
+    res.send("user updated successfully!!");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
+});
+//get all the users from DB
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
 });
 connectDb()
   .then(() => {
